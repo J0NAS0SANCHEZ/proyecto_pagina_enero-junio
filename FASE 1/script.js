b735d82caeb89b74e126475ca1a29f9d1834d529
J0NAS0SANCHEZ <128201674+J0NAS0SANCHEZ@users.noreply.github.com>
2024-02-16 19:22:02 -0600

    Add files via upload

---
+++ FASE 1/script.js
@@ -0,0 +1,56 @@
+const btnLeft = document.querySelector(".btn-left"),
+      btnRight = document.querySelector(".btn-right"),
+      slider = document.querySelector("#slider"),
+      sliderSection = document.querySelectorAll(".slider-section");
+
+btnLeft.addEventListener("click", e => moveToLeft())
+btnRight.addEventListener("click", e => moveToRight() )
+
+let operacion = 0,
+    couter = 0,
+    widthImg = 100 / sliderSection.length;
+
+function moveToRight() {
+    if (couter >= sliderSection.length-1) {
+        couter = 0
+        operacion = 0
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "none"
+        return;
+    }
+
+    couter++
+    operacion = operacion + widthImg;
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease.6s"
+}
+
+function moveToLeft() {
+    couter --;
+    if (couter < 0) {
+        couter = sliderSection.length-1;
+        operacion = widthImg * (sliderSection.length-1)
+
+        slider.style.transform = `translate(-${operacion}%)`
+        slider.style.transition = "none"
+        return;
+    }
+
+    operacion = operacion - widthImg;
+    slider.style.transform = `translate(-${operacion}%)`
+    slider.style.transition = "all ease.6s"
+}
+ //*********************************************************************************************/
+//CATALOGO 
+
+const fulimgbox = document.getElementById("fulimgbox"),
+fulimg = document.getElementById("fulimg");
+
+function OpenFullClick(reference) {
+    fulimgbox.style.display = "flex";
+    fulimgbox.src = reference
+}
+
+function closeimg () {
+    fulimgbox.style.display = "none";
+}
