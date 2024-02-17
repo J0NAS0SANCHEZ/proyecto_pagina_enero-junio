9ab3fd3370e26da200e534b7338f048b8ebf0b06
J0NAS0SANCHEZ <128201674+J0NAS0SANCHEZ@users.noreply.github.com>
2024-02-17 17:19:40 -0600

    Actualizacion #2 AUTOMATIC SLIDER script.js

--- FASE 1/script.js
+++ FASE 1/script.js
@@ -8,6 +8,10 @@ const btnLeft = document.querySelector(".btn-left"),
 btnLeft.addEventListener("click", e => moveToLeft())
 btnRight.addEventListener("click", e => moveToRight() )
 
+setInterval(() => {
+    moveToRight()
+},3000);
+
 let operacion = 0,
     couter = 0,
     widthImg = 100 / sliderSection.length;
