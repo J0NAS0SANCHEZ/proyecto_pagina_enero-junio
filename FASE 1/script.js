ecf291b71c6c963c52ecee75dbc86a1588dac475
J0NAS0SANCHEZ <128201674+J0NAS0SANCHEZ@users.noreply.github.com>
2024-02-29 22:36:01 -0600

    ACTUALIZACION GENERAL #1  script.js
    Se añadió funciones ligadas con el navbar para ocultar la tarjeta cada vez que se utilice el dropdown

--- FASE 1/script.js
+++ FASE 1/script.js
@@ -2,15 +2,22 @@ const btnLeft = document.querySelector(".btn-left"),
       btnRight = document.querySelector(".btn-right"),
       slider = document.querySelector("#slider"),
       sliderSection = document.querySelectorAll(".slider-section"),
-      fulimgbox = document.getElementById("fulimgbox"),
-      fulimg = document.getElementById("fulimg");
+      fulimgbox = document.getElementById("fulimgbox"),//CATALOGO
+      fulimg = document.getElementById("fulimg"),
+      img1 = document.querySelector(".img1"),
+      text = document.getElementById("text");
+
+
 
 btnLeft.addEventListener("click", e => moveToLeft())
-btnRight.addEventListener("click", e => moveToRight() )
+btnRight.addEventListener("click", e => moveToRight())
+
 
 setInterval(() => {
     moveToRight()
 },3000);
+
+
 
 let operacion = 0,
     couter = 0,
@@ -46,16 +53,44 @@ function moveToLeft() {
     slider.style.transform = `translate(-${operacion}%)`
     slider.style.transition = "all ease.6s"
 }
- //*********************************************************************************************/
+
+//***********************TARJETA**************************************************************** */
+
+
+ // JavaScript para ocultar la tarjeta cuando se despliega el menú
+ document.addEventListener("DOMContentLoaded", function() {
+    var navbarToggle = document.querySelector(".navbar-toggler");
+    var cardContainer = document.getElementById("cardContainer");
+
+    navbarToggle.addEventListener("click", function() {
+      if (cardContainer.style.display !== "none") {
+        cardContainer.style.display = "none";
+      } else {
+        cardContainer.style.display = "block";
+      }
+    });
+
+    // Ocultar la tarjeta cuando se hace clic en un enlace de la barra de navegación
+    var navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    navLinks.forEach(function(navLink) {
+      navLink.addEventListener("click", function() {
+        cardContainer.style.display = "none";
+      });
+    });
+  });
+
+
+  //*********************************************************************************************/
 //CATALOGO 
 
 
-
-function OpenFullClick(reference) {
+function OpenFullClick(reference) 
+{
     fulimgbox.style.display = "flex";
     fulimg.src = reference
 }
 
-function closeimg () {
+function closeimg()
+{
     fulimgbox.style.display = "none";
 }
